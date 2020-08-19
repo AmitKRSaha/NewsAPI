7d687427453d48a5d90f946807112789f6b4e2b9
AKSAHA <sheol.aks@gmail.com>
2020-08-20 04:09:19 +0530

    updated Readme

--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -1,25 +1,25 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 
-function SearchItem(props) {
+function SearchItem({ SearchbyAuthor }) {
 
     const [author, setAuthor] = useState('');
     const inputRef = useRef('');
     useEffect(() => {
         let timer = setTimeout(() => {
             if (author === inputRef.current.value) {
-                props.SearchbyAuthor(author);
+                SearchbyAuthor(author);
             }
         }, 500);
         return () => clearInterval(timer);
-    }, [author]);
+    }, [author, SearchbyAuthor]);
 
     return (
         <div className="input-group mb-3">
             <input type="text"
                 className=" input-control form-control"
-                placeholder="Recipient's username"
-                aria-label="Recipient's username"
+                placeholder="Please provide author name"
+                aria-label="Please provide author name"
                 aria-describedby="button-addon2"
                 value={author}
                 ref={inputRef}
